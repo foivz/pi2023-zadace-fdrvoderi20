080d4fc3cd51abde736e25b057451ddbf87a4a7b
Filip Drvoderić <fdrvoderi20@foi.hr>
2023-05-28 13:11:40 +0200

    Implementation of hierarchy

---
+++ Software/ERA_Manager/ERA_Manager/Models/EosmManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERA_Manager.Models
+{
+    internal class EosmManager
+    {
+    }
+}
---
+++ Software/ERA_Manager/ERA_Manager/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERA_Manager.Models
+{
+    public class Student
+    {
+        public string Motivation { get; set; }
+        public string Preferences { get; set; }
+    }
+}
